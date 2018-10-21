3991853e769691dc4e51f0fc8ff8ad5c4b2efcb1
Anuj Panchal <anujpan1@gmail.com>
2018-10-21 23:37:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,9 @@ require 'pry'
 class Song
   attr_accessor :name, :artist, :genre
   
-  @@count = 0
-  @@artists = []
-  @@genres = []
+  # @@count = 0
+  # @@artists = []
+  # @@genres = []
   
   def initialize(name, artist, genre)
     # @name = name
