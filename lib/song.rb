705c0d4bd5f7da7cbf1ef1da06c7666dd0485440
Anuj Panchal <anujpan1@gmail.com>
2018-10-21 23:36:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,44 +18,44 @@ class Song
     @@artists.push(artist)
   end
   
-  def self.count
-    return @@count
-  end
+  # def self.count
+  #   return @@count
+  # end
   
-  def self.artists
-    @@artists.uniq
-  end
+  # def self.artists
+  #   @@artists.uniq
+  # end
   
-  def self.genres
-    @@genres.uniq
-  end
+  # def self.genres
+  #   @@genres.uniq
+  # end
   
-  def self.genre_count
-    genre_hash = {}
+  # def self.genre_count
+  #   genre_hash = {}
     
-    @@genres.each do |genre|
-      if genre_hash[genre]
-        genre_hash[genre] += 1
-      else
-        genre_hash[genre] = 1
-      end
-    end
+  #   @@genres.each do |genre|
+  #     if genre_hash[genre]
+  #       genre_hash[genre] += 1
+  #     else
+  #       genre_hash[genre] = 1
+  #     end
+  #   end
     
-    genre_hash
-  end
+  #   genre_hash
+  # end
   
-  def self.artist_count
-    artist_hash = {}
+  # def self.artist_count
+  #   artist_hash = {}
     
-    @@artists.each do |artist|
-      if artist_hash[artist]
-        artist_hash[artist] += 1
-      else
-        artist_hash[artist] = 1
-      end
-    end
+  #   @@artists.each do |artist|
+  #     if artist_hash[artist]
+  #       artist_hash[artist] += 1
+  #     else
+  #       artist_hash[artist] = 1
+  #     end
+  #   end
     
-    artist_hash
-  end
+  #   artist_hash
+  # end
 end
 
